e926c05000b58f05c5128941019ba43b530052e4
Mandy <Lmandy239@gmail.com>
2023-11-19 18:17:52 -0500

    Dried out the logic and created methods to separate a bit

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # MakeChangeProject
 
 ## How To Run
-
+Simply follow the prompt and input proper amounts.
 ## Project Description
-
+Just a simple machine that takes in a price and returns a change.
 ## Technologies Used
-
+Java
 ## Lessons Learned
+ I learned to carefully read the prompt before doing anything, at first I started by making a sort of menu with items in which it would ask the user to input money and return change. But that didnt meet the requirements of the user prompts. I believe it was a language barried, as I didnt understand what it meant by tendered.
--- src/makechange/MakeChangeHW.java
+++ src/makechange/MakeChangeHW.java
@@ -4,56 +4,74 @@ import java.util.Scanner;
 
 public class MakeChangeHW {
 	public static void main(String[] args) {
-		
 		calculatedIfs();
 
-
 	}
-	
 	public static void calculatedIfs() {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter the price of an item: ");
-		double priceOfItem = keyboard.nextDouble();
-		System.out.println("Enter the amount to be tendered: ");
-		double amountTendered = keyboard.nextDouble();
-		
-		int priceInCents = (int) (priceOfItem * 100);
-		int amountInCents = (int) (amountTendered * 100);
-		int priceInDime = (int) (priceOfItem * 10);
-		int amountInDime = (int) (amountTendered * 10);
-		int priceInQuarter = (int) (priceOfItem * 4);
-		int amountInQuarter = (int) (amountTendered * 4);
-		int priceInDollar = (int) (priceOfItem);
-		int amountInDollar = (int) (amountTendered);
-		
-		int changeInCents = amountInCents - priceInCents;
-		int changeInDimes = amountInDime - priceInDime;
-		int changeInQuarter = amountInQuarter - priceInQuarter;
-		int changeInDollar = amountInDollar - priceInDollar;
-		
-		if (amountTendered < priceOfItem) {
-			System.out.println("Your amount is insufficient! Please enter a higher amount.");
-		} else if (amountTendered == priceOfItem) {
-			System.out.println("No change! Exact amount entered!!");
-		} else {
-			if (amountTendered < 0.10) {
-				System.out.println(changeInCents + " cent");
-			} else if (amountTendered < 1.0) {
-				if (amountTendered % 0.25 == 0) {
-					System.out.println(changeInQuarter + " quarter");
-				} else {
-					System.out.println(changeInDimes + " dime");
-				}
-			}
-			if (amountTendered > 1.0 && amountTendered < 20.01)
-				if (changeInDollar % 10.0 == 0 || changeInDollar % 05.0 == 0) {
-					System.out.println(changeInDollar + " dollar bill");
-				} else {
-					System.out.println(changeInDollar + " dollars");
-				}
-			
-		}
-		keyboard.close();
-	}
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the price of an item: ");
+        double priceOfItem = keyboard.nextDouble();
+        System.out.println("Enter the amount to be tendered: ");
+        double amountTendered = keyboard.nextDouble();
 
+        int priceInCents = (int) (priceOfItem * 100);
+        int amountInCents = (int) (amountTendered * 100);
+
+        int changeInCents = amountInCents - priceInCents;
+
+        if (amountTendered < priceOfItem) {
+            System.out.println("Your amount is insufficient! Please enter a higher amount.");
+        } else if (amountTendered == priceOfItem) {
+            System.out.println("No change! Exact amount entered!!");
+        } else {
+            handleDollars(changeInCents);
+            handleQuarters(changeInCents);
+            handleDimes(changeInCents);
+            handleCents(changeInCents);
+        }
+    }
+
+    private static void handleDollars(int changeInCents) {
+        int dollars = changeInCents / 100;
+        if (dollars > 0) {
+            if (dollars == 1) {
+                System.out.println(dollars + " dollar");
+            } else {
+                System.out.println(dollars + " dollars");
+            }
+        }
+    }
+
+    private static void handleQuarters(int changeInCents) {
+        int quarters = (changeInCents % 100) / 25;
+        if (quarters > 0) {
+            if (quarters == 1) {
+                System.out.println(quarters + " quarter");
+            } else {
+                System.out.println(quarters + " quarters");
+            }
+        }
+    }
+
+    private static void handleDimes(int changeInCents) {
+        int dimes = ((changeInCents % 100) % 25) / 10;
+        if (dimes > 0) {
+            if (dimes == 1) {
+                System.out.println(dimes + " dime");
+            } else {
+                System.out.println(dimes + " dimes");
+            }
+        }
+    }
+
+    private static void handleCents(int changeInCents) {
+        int cents = ((changeInCents % 100) % 25) % 10;
+        if (cents > 0) {
+            if (cents == 1) {
+                System.out.println(cents + " cent");
+            } else {
+                System.out.println(cents + " cents");
+            }
+        }
+    }
 }
