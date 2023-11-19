a1a894e96733300496fcb2e63cff1ec0b3d3d2a9
Mandy <Lmandy239@gmail.com>
2023-11-19 00:06:28 -0500

    Change values, added new variables

--- src/makechange/MakeChangeHW.java
+++ src/makechange/MakeChangeHW.java
@@ -5,47 +5,45 @@ import java.util.Scanner;
 public class MakeChangeHW {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		double priceOfItem = 0;
-		double amountTendered = 0;
-		int twentyDollarBill = 20;
-		int tenDollarBill = 10;
-		double fiveDollarBill = 5;
-		double oneDollarBill = 1;
-		double oneQuarter = 0.25;
-		double oneDime = 0.10;
+		int centValue = 1;
+		int dimeValue = 10;
+		int nickelValue = 5;
+		int dollarValue = 100;
+		int fiveDollarValue = 500;
+		int tenDollarValue = 1000;
+		int twentyDollarValue = 2000;
 
 		System.out.println("Enter the price of an item: ");
-		priceOfItem = keyboard.nextDouble();
+		double priceOfItem = keyboard.nextDouble();
 		System.out.println("Enter the amount to be tendered: ");
-		amountTendered = keyboard.nextDouble();
-		int change1 = (int) (amountTendered - priceOfItem);
+		double amountTendered = keyboard.nextDouble();
+
+		int priceInCents = (int) (priceOfItem * 100);
+		int amountInCents = (int) (amountTendered * 100);
+		int priceInDime = (int) (priceOfItem * 10);
+		int amountInDime = (int) (amountTendered *10);
+		int priceInNickle = (int) (priceOfItem * 4);
+		int amountInNickle = (int) (amountTendered * 4);
+        int priceInDollar = (int) (priceOfItem);
+        int amountInDollar = (int) (amountTendered);
+        
+		int changeInCents = amountInCents - priceInCents;
+		int changeInDimes = amountInDime - priceInDime;
+		int changeInNickle = amountInNickle - priceInNickle;
+		int changeInDollar = amountInDollar - priceInDollar;
+		
+				
+
+		if (changeInCents < 0) {
+			System.out.println("Your amount is insufficient! Please enter a higher amount.");
+		} else if (changeInCents == 0) {
+			System.out.println("No change! Exact amount entered!!");
+		} else {
+			System.out.println("Change to be given: " + changeInCents + " cents");
+
+		}
+
 		keyboard.close();
-		
-		if (amountTendered < priceOfItem) {
-			System.out.println("Your amount is insuficient! please enter a higher amount.");
-		}
-
-		else if (amountTendered == priceOfItem) {
-			System.out.println("No change!, Exact amount entered!!");
-		}
-
-		else {
-		
-			if (change1 >= 20) {
-			  twentyDollarBill = change1 / twentyDollarBill; 
-			  if (twentyDollarBill >= 2) {
-				   change1 = 20;
-				  System.out.println(twentyDollarBill + ": " + change1 + " dollar bills");
-			  } else if (twentyDollarBill == 1) {
-				  System.out.println("1: 20 dollar bill");
-			  }
-			  change1 %= 20;
-			}
-		
-			
-			
-			
-		}
 	}
 
 }
